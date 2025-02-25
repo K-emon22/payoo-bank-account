a006bbc6b750eb0d5c4161574f5830130317db3a
K-emon22 <emonsheikhkhalid2@gmail.com>
2025-02-25 14:31:43 +0600

    logout

--- js/index.js
+++ js/index.js
@@ -9,15 +9,14 @@ document
     const convertedPin = parseFloat(password);
 
     if (accountNum.length === 11 && accountNum.startsWith("01")) {
+      if (convertedPin === 1234) {
+        window.location.href = "main.html";
+      } else {
+        alert("wrong password");
+        // document.getElementById("password").style.border = "2px solid red ";
+      }
     } else {
-      document.getElementById("account-num").style.border = "2px solid red ";
-     
-    }
-
-    if (convertedPin === 1234) {
-         window.location.href="main.html"
-    } else {
-      console.log("wrong password");
-      document.getElementById("password").style.border = "2px solid red ";
+      // document.getElementById("account-num").style.border = "2px solid red";
+      alert("Enter a valid number");
     }
   });
